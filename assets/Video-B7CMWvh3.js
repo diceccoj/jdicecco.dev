a03a1c02d90d38354dd3dab21d3e9cf7e5bcd12f
Jonathan DiCecco <diceccoj@mcmaster.ca>
2024-06-15 14:55:46 -0400

    pbi

---
+++ assets/Video-B7CMWvh3.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CiGW1Fcw.js";const e=({src:o})=>s.jsx("video",{controls:!0,src:o,className:"bg-slate-300 dark:bg-slate-600 rounded-lg p-3 m-5 dynamic-opacity hover:shadow-xl hover:scale-105 transition-all w-[70%]"});export{e as V};
