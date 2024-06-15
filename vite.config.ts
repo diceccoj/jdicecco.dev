2bae254e698df4e748558a24f425e782c2f24641
Jonathan DiCecco <diceccoj@mcmaster.ca>
2024-06-15 14:47:52 -0400

    pbi

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ const outDir = resolve(__dirname, 'dist');
 const pages = resolve(__dirname, "src", "pages");
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jdicecco.dev",
+  base: "",
   plugins: [react()],
   build : {
     outDir,
