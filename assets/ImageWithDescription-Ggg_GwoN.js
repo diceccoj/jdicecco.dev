34d1d38a49ceaacb5e385642dde9ce8577d2c69e
Jonathan DiCecco <diceccoj@mcmaster.ca>
2024-06-16 11:36:13 -0400

    Updates

---
+++ assets/ImageWithDescription-Ggg_GwoN.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-DWpXzhes.js";import{R as t}from"./SkillsPanel-CQ0UfFrM.js";const o=({src:e,image_size:a="large",bonus_classes:m="",children:r=""})=>s.jsxs(s.Fragment,{children:[s.jsx(t,{src:e,size:a,bonus_classes:m+" lg:w-[100%] w-[70%]"}),s.jsx("p",{className:"small-text-bg lg:w-[90%] w-[65%] m-4 lg:mb-4 mb-20",children:r})]});export{o as I};
