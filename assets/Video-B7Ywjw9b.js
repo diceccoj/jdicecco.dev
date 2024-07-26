630a2b4472272b1912008f77ba476f9f9d677697
Jonathan DiCecco <diceccoj@mcmaster.ca>
2024-07-26 14:33:31 -0400

    Updates

--- assets/Video-B7Ywjw9b.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-DWpXzhes.js";const e=({src:o})=>s.jsx("video",{controls:!0,src:o,className:"bg-slate-300 dark:bg-slate-600 rounded-lg p-3 m-5 dynamic-opacity hover:shadow-xl hover:scale-105 transition-all w-[70%]"});export{e as V};
