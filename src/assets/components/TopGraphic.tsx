cc9c799f6699e64b0c55c2b9c9f302caa2ff1aaa
diceccoj <107959943+diceccoj@users.noreply.github.com>
2025-11-30 14:14:59 -0500

    Updated resume and top graphic

--- src/assets/components/TopGraphic.tsx
+++ src/assets/components/TopGraphic.tsx
@@ -11,33 +11,17 @@ const TopGraphic = () => {
             <RoundImage src="/portrait.png" size="medium" />
           </div>
           <p className="m-5 small-text-bg w-fit p-5 ">
-            Hi! My name is Jonathan DiCecco. I am a second year Computer Science
-            Student at McMaster University. My favourite thing about CS is the
-            ability to make something new. I like being able to come up with an
-            interesting idea for a game, website or program. If no one else made
-            it, then why not try to make it yourself? Below you'll find some
-            examples of that.
+            Hi! My name is Jonathan DiCecco. I am a fourth year Computer Science Student at McMaster University. My favourite thing about CS is the ability to make something new. I like being able to
+            come up with an interesting idea for a game, website or program. If no one else made it, then why not try to make it yourself? Below you'll find some examples of that.
           </p>
         </div>
       </div>
 
       <div className="flex justify-center mt-10 ">
         <div className="responsive-grid-3 place-items-center w-1/2">
-          <SocialMediaButton
-            title="Github Page"
-            url="https://github.com/diceccoj"
-            image="github.png"
-          />
-          <SocialMediaButton
-            title="Linkedin"
-            url="https://www.linkedin.com/in/jonathan-dicecco-b9529a234/"
-            image="linkedin.png"
-          />
-          <SocialMediaButton
-            title="Resume"
-            url="/resume.pdf"
-            image="resume.png"
-          />
+          <SocialMediaButton title="Github Page" url="https://github.com/diceccoj" image="github.png" />
+          <SocialMediaButton title="Linkedin" url="https://www.linkedin.com/in/jonathan-dicecco-b9529a234/" image="linkedin.png" />
+          <SocialMediaButton title="Resume" url="/resume.pdf" image="resume.png" />
         </div>
       </div>
     </RevealOnScroll>
